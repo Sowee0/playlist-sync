cbfbfe47e06516d02f3e11fb98b024d4e560d578
Gabriel Oliveira <sowee00@gmail.com>
2022-09-27 21:58:50 -0300

    Adding the python version

---
+++ sync_python.py
@@ -0,0 +1,10 @@
+import spotipy
+import ytmusicapi
+import json
+
+#Loading the JSON with user credentials for both services
+user_file = open("credentials.json")
+user_data = json.load(user_file)
+
+
+
